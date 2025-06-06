113059d6538b47404b19d764b1eae51816d912a7
Luis Kinn DC <luiskinndc@gmail.com>
2025-06-06 17:06:33 -0500

    quizzes

--- app/src/main/java/com/app/aprendequechua/fragments/QuizFragment.kt
+++ app/src/main/java/com/app/aprendequechua/fragments/QuizFragment.kt
@@ -5,10 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.app.aprendequechua.R
 import com.google.android.material.chip.Chip
+import com.google.firebase.firestore.FirebaseFirestore
 
 class QuizFragment : Fragment() {
+
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,12 +26,36 @@ class QuizFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val IrQuizBasico = view.findViewById<Chip>(R.id.IrQuizBasico)
+        db = FirebaseFirestore.getInstance()
 
+        // Consultar la cantidad de quizzes disponibles
+        loadQuizCount { quizCount ->
+            if (quizCount != null) {
+                IrQuizBasico.text = "$quizCount quizzes disponibles"
+            } else {
+                IrQuizBasico.text = "No hay quizzes disponibles"
+            }
+        }
+
+        // Configurar el clic en el Chip
         IrQuizBasico.setOnClickListener {
             parentFragmentManager.beginTransaction()
                 .replace(R.id.fragmentContainer, ResolverQuizFragment())
                 .addToBackStack(null) // Para que pueda regresar con el botón atrás
                 .commit()
         }
+    }
+
+    private fun loadQuizCount(onSuccess: (Int?) -> Unit) {
+        db.collection("quizzes_basico") // Asegúrate de que esta sea la colección correcta
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                val quizCount = querySnapshot.size()
+                onSuccess(quizCount)
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, "Error al cargar quizzes: ${exception.message}", Toast.LENGTH_SHORT).show()
+                onSuccess(null)
+            }
     }
 }
---
+++ app/src/main/java/com/app/aprendequechua/models/QuizModel.kt
@@ -0,0 +1,12 @@
+package com.app.aprendequechua.models
+
+data class QuizModel(
+    val pregunta: String = "",
+    val opcion1: String = "",
+    val opcion2: String = "",
+    val opcion3: String = "",
+    val opcion4: String = "",
+    val dificultad: String = "",
+    val respuesta_correcta: String = "",
+    val pista: String = ""
+)
